7d32c02130b83f02b8678885345289b3e85a67e9
Codingwal <florian.herzog08@gmail.com>
2024-01-21 19:46:35 +0100

    SchrotterCode

--- m433_receive_b/m433_receive_b.ino
+++ m433_receive_b/m433_receive_b.ino
@@ -1,6 +1,12 @@
 #include <RCSwitch.h>
-
 RCSwitch mySwitch = RCSwitch();
+
+unsigned long data;
+unsigned long prevData;
+unsigned char head;
+unsigned char prevHead = -1;
+
+char prevChar;
 
 void setup() {
   Serial.begin(9600);
@@ -9,19 +15,33 @@ void setup() {
   digitalWrite(3, LOW);
 }
 
-void loop() {
+bool receiveData() {
   if (!mySwitch.available()) {
+    return false;
+  }
+
+  const unsigned long msg = mySwitch.getReceivedValue();
+  data = msg & 0xFFFFFF;
+  head = msg >> 24;
+
+  if (head == prevHead) {
+    return false;
+  }
+  prevHead = head;
+  prevData = data;
+
+  return true;
+}      
+
+void loop() {
+  if (!receiveData()) {
     return;
   }
-  const unsigned int *msg = mySwitch.getReceivedRawdata();
-
-  print((char *)msg, mySwitch.getReceivedBitlength());
+   
+  // Serial.println(head);
+  Serial.println(head);
+  Serial.println(data);
+  Serial.println();
 
   mySwitch.resetAvailable();
-}
-
-void print(const char *msg, unsigned int size) {
-  for (int i = 0; i < size; i += 8) {
-    Serial.print(msg[i]);
-  }
 }
--- m433_send_a/m433_send_a.ino
+++ m433_send_a/m433_send_a.ino
@@ -5,14 +5,41 @@
 Adafruit_BMP280 bmp;
 RCSwitch mySwitch = RCSwitch();
 
+float temp_arr[1000];
+float pres_arr[1000];
+
+int i = 0;
+
 void setup() {
   mySwitch.enableTransmit(10);  // Der Sender wird an Pin 10 angeschlossen
+
+  bmp.begin(2);
 
   Serial.begin(9600);
 }
 void loop() {
+  float temp = getTemp();
+  temp_arr[i] = temp;
 
-  mySwitch.send("Hello, World");
+  sendData(100, 0);
+  delay(1000);
+  sendData(999, 1);
 
   delay(1000);
+}
+
+void sendData(unsigned long data, unsigned char head)
+{
+  mySwitch.send(((unsigned long)head << 24) + (data & 0xFFFFFF), 32);
+}
+
+float getTemp()
+{
+  float temp = bmp.readTemperature();
+  return temp;
+}
+float getPres()
+{
+  float pres = bmp.readPressure();
+  return pres;
 }

e3a7e4795ccda0c3decc94f096b00df8091fb4a8
TheRealNachtfalter <142906890+TheRealNachtfalter@users.noreply.github.com>
2024-03-01 16:06:11 +0100

    Update bodenstation.js

--- bodenstation.js
+++ bodenstation.js
@@ -262,7 +262,9 @@ let rawSDdataProceded = false;
 async function importFromSD() {
     setInterval(() => {
         console.log('Interval');
-        document.body.innerHTML = (sdraw.length / 1000).toFixed(3) + 'KB';
+        if (!rawSDdataProceded) {
+            document.body.innerHTML = (sdraw.length / 1000).toFixed(3) + 'KB';
+        }
         if (importCompleted()) {
             if (!rawSDdataProceded) {
                 processSDRawData(sdraw);

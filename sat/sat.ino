dae3a987aa699232f4605a9edccd30a505e008b9
TheRealNachtfalter <142906890+TheRealNachtfalter@users.noreply.github.com>
2024-03-08 22:12:31 +0100

    Update sat.ino

--- sat/sat.ino
+++ sat/sat.ino
@@ -1,10 +1,9 @@
 #include <Adafruit_BMP280.h>
 #include <Adafruit_BNO055.h>
 #include <SoftwareSerial.h>
-//#include <TinyGPSPlus.h>
-//#include <TinyGPSMinus.h>
 #include <RH_RF95.h>
-#include <SdFat.h>
+#include <SD.h>
+#include <SPI.h>
 
 // SPI Pins Arduino Nano: CS 10, MOSI 11, MISO 12, SCK 13
 // https://www.arduino.cc/reference/en/language/functions/communication/spi/
@@ -15,7 +14,7 @@
 #define BNO055_I2C_ADDRESS 0x28
 
 // PIN Belegungen
-#define GPS_TX_PIN 2 // Es beleiben PIN D2 und D3 für das GPS-Modul übrig
+#define GPS_TX_PIN 2  // Es beleiben PIN D2 und D3 für das GPS-Modul übrig
 #define GPS_RX_PIN 3
 #define FAN_PIN 4
 #define LORA_RX_PIN 5
@@ -40,14 +39,14 @@
 
 #define STARTALTITUDE 30  // Höhe vor dem Start vom Flugplatz
 
-#define TOLERANCE 5 // Toleranz in Metern, um Höhenunterschiede mit dem BMP zu messen
-#define FAN_STARTING_HEIGHT 400 // Ab welcher Höhe der Lüfter den Airbag aufblässt
-#define BMP_ALTITUDES_SIZE 8 // Wie viele letzte Temperaturen gespeichert werden sollen
+#define TOLERANCE 5              // Toleranz in Metern, um Höhenunterschiede mit dem BMP zu messen
+#define FAN_STARTING_HEIGHT 400  // Ab welcher Höhe der Lüfter den Airbag aufblässt
+#define BMP_ALTITUDES_SIZE 8     // Wie viele letzte Temperaturen gespeichert werden sollen
+
+File file;
 
 Adafruit_BMP280 bmp;
 Adafruit_BNO055 bno(55, BNO055_I2C_ADDRESS, &Wire);
-// TinyGPSPlus gps;
-//TinyGPSMinus gps;
 SoftwareSerial ss(GPS_RX_PIN, GPS_TX_PIN);
 SoftwareSerial rf(LORA_RX_PIN, LORA_TX_PIN);
 RH_RF95 rf95(rf);
@@ -64,64 +63,66 @@ int counter = 0;
 
 long time;
 
-SdFat32 SD;
-File32 file;
-
 void setup() {
   Serial.begin(9600);
   Serial.println(100);
+
+  pinMode(SD_CS_PIN, OUTPUT);
+  if (!SD.begin(SD_CS_PIN)) {  // Init SD (Speicher)
+    error(ERROR_SD_CONNECT);
+  }
+
+  // Datei mit höchstem Wert als Namen finden und dann mit Wert + 1 als Namen eine Datei erstellen
+  // So muss nicht nach jedem Test die SD Karte geleert werden, sondern die Dateien sind chronologisch sortiert
+  int filecounter = 0;
+  while (SD.exists(String(filecounter))) {
+    filecounter++;
+  }
+  file = SD.open(String(filecounter), FILE_WRITE);
+  //file = SD.open("data.txt", FILE_WRITE);
+  if (file) {
+    file.println("");  // Irgendetwas in die erste Zeile geschrieben werden, damit die Zahlen gespeichert werden können
+  } else {
+    error(ERROR_SD_OPEN);
+  }
 
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
 
   pinMode(FAN_PIN, OUTPUT);
   digitalWrite(FAN_PIN, LOW);
-
-  /*if (!bmp.begin(BMP280_I2C_ADDRESS)) { // Init BMP (Luftdruck & Temperatur)
+  Serial.println(1);
+  if (!bmp.begin(BMP280_I2C_ADDRESS)) {  // Init BMP (Luftdruck & Temperatur)
     error(ERROR_BMP);
-  }*/
+  }
   sealevelhpa = bmp.seaLevelForAltitude(STARTALTITUDE, bmp.readPressure());
-
-  if (!bno.begin()) { // Init BNO (Inertialplatform)
+  if (!bno.begin()) {  // Init BNO (Inertialplatform)
     error(ERROR_BNO);
   }
-  if (!rf95.init()) { // RF95 (Funkmodul) initialisieren
+  Serial.println(2);
+  rf.listen();
+  if (!rf95.init()) {  // RF95 (Funkmodul) initialisieren
     error(ERROR_RF95);
   }
+  Serial.println(3);
+  Serial.println(4);
+  Serial.println(5);
   rf95.setFrequency(FREQUENCY);
 
-  ss.begin(9600); // GPS
-
-  if (!SD.begin(SD_CS_PIN)) { // Init SD (Speicher)
-    error(ERROR_SD_CONNECT);
-  }
-  /*{
-    // Datei mit höchstem Wert als Namen finden und dann mit Wert + 1 als Namen eine Datei erstellen
-    // So muss nicht nach jedem Test die SD Karte geleert werden, sondern die Dateien sind chronologisch sortiert
-    int filecounter = 0;
-    while (SD.exists(String(filecounter))) {
-      filecounter++;
-    }
-    file = SD.open(String(filecounter), FILE_WRITE);
-    if (file) {
-      file.println(""); // Irgendetwas in die erste Zeile geschrieben werden, damit die Zahlen gespeichert werden können
-    } else {
-      error(ERROR_SD_OPEN);
-    }
-  }*/
-
   Serial.println(200);
-  digitalWrite(LED_PIN, HIGH); // LED an
+  digitalWrite(LED_PIN, HIGH);  // LED an
   time = millis();
 
+  ss.begin(9600);              // GPS
+
   // Nur bei genügend Speicher einbauen
-  /*  tone(SPEAKER_PIN, 200);
-      delay(100);
-      tone(SPEAKER_PIN, 400);
-      delay(100);
-      tone(SPEAKER_PIN, 600);
-      delay(100);
-      noTone(SPEAKER_PIN);*/
+  tone(SPEAKER_PIN, 200);
+  delay(100);
+  tone(SPEAKER_PIN, 400);
+  delay(100);
+  tone(SPEAKER_PIN, 600);
+  delay(100);
+  noTone(SPEAKER_PIN);
 }
 
 void loop() {
@@ -130,9 +131,8 @@ void loop() {
   send(DATA_BLOCK_START);
   send(counter);
 
-  { // BMP
-    //float pressure = bmp.readPressure();
-
+  {
+    // BMP
     bmp_altitude = 44300 * (1 - (pow(bmp.readPressure() / sealevelhpa, 0.1903)));
 
     send(bmp.readTemperature());  // Temperatur senden
@@ -152,7 +152,13 @@ void loop() {
     send(gyroscope.gyro.z);
   }
 
-  { // GPS
+  ss.listen();
+  while (ss.available() > 0) {
+    Serial.write(ss.read());
+  }
+  rf.listen();
+
+  /*{ // GPS
     ss.listen();
     float latitude, longitude, altitude;
     if (!landed) {
@@ -194,21 +200,23 @@ void loop() {
       latitude = gpsdata[0];
       longitude = gpsdata[0];
       altitude = gpsdata[4];
-    }
+    }*/
 
-    rf.listen();
-    send(latitude);
-    send(longitude);
-    send(altitude);
-  }
+  rf.listen();
+  //send(latitude);
+  //send(longitude);
+  //send(altitude);
+  //}
+
+
 
   { // Fumktion zum schauen, ob ausgeworfen oder gelandet
     short x = bmp_altitudes[counter % BMP_ALTITUDES_SIZE] - TOLERANCE;
-    if (counter > BMP_ALTITUDES_SIZE) { // Erst mit Vergleichen beginnen, wenn die Liste an Vergleichswerten voll ist
-      if (x > bmp_altitude) { // Falls die Höhe fällt
+    if (counter > BMP_ALTITUDES_SIZE) {  // Erst mit Vergleichen beginnen, wenn die Liste an Vergleichswerten voll ist
+      if (x > bmp_altitude) {            // Falls die Höhe fällt
         ejected = true;
-      } else if (x < bmp_altitude) { // Falls die Höhe steigt oder eher gleichbleit, dafür ist die Toleranz da
-        if (ejected == true) { // Falls schon ausgeworfen, muss also gelandet sein :)
+      } else if (x < bmp_altitude) {  // Falls die Höhe steigt oder eher gleichbleit, dafür ist die Toleranz da
+        if (ejected == true) {        // Falls schon ausgeworfen, muss also gelandet sein :)
           landed = true;
         }
       }
@@ -217,8 +225,8 @@ void loop() {
 
   // Serial.println(analogRead(VOLTAGE_PIN) / 1023 * 5 * 2); // Spannungsüberwachung
 
-  if (ejected) {// Falls ausgeworfen, piept der akustische Signalgeber
-    tone(SPEAKER_PIN, 1000, 500); // 0.5 Sekunden den Ton abspielen
+  if (ejected) {                   // Falls ausgeworfen, piept der akustische Signalgeber
+    tone(SPEAKER_PIN, 1000, 500);  // 0.5 Sekunden den Ton abspielen
   }
 
   if (bmp_altitude < FAN_STARTING_HEIGHT && ejected) {
@@ -226,43 +234,43 @@ void loop() {
     digitalWrite(FAN_PIN, HIGH);
   }
 
-  if (landed) { // Falls geladet, Lüfter ausschalten
+  if (landed) {  // Falls geladet, Lüfter ausschalten
     digitalWrite(FAN_PIN, LOW);
   }
 
   send((fan << 0) + (ejected << 1) + (landed << 2));  // Sendet alle Bools in einem Byte
 
-
   file.println(millis() - time, DEC);
-  time = millis(); // Zeit zurücksetzten, um jedesmal die Zeit, die die Schleife (loop()) gebraucht hat zu bestimmen
+  time = millis();  // Zeit zurücksetzten, um jedesmal die Zeit, die die Schleife (loop()) gebraucht hat zu bestimmen
 
   send(DATA_BLOCK_END);
 
-  bmp_altitudes[counter % BMP_ALTITUDES_SIZE] = bmp_altitude; // Aktueller Höhenmeter-Wert speichern
+  bmp_altitudes[counter % BMP_ALTITUDES_SIZE] = bmp_altitude;  // Aktueller Höhenmeter-Wert speichern
 
   counter++;
 }
 
 void send(float val) {
   {
-    uint8_t tosend[sizeof(float)]; // Ändert den Typ von float zu uint8_t[], ohne tatsächlich Bits zu modifizieren
+    uint8_t tosend[sizeof(float)];  // Ändert den Typ von float zu uint8_t[], ohne tatsächlich Bits zu modifizieren
     memcpy(&tosend, &val, sizeof(float));
     rf95.send(tosend, sizeof(float));
   }
+  //Serial.println(val);
   {
-    //file.println(val, DEC);
-    //file.flush(); // Tatsächlich Speichern, wäre ansonsten evtl. nur im Buffer, was zu Fehlern führen kann :(
+    file.println(val, DEC);
+    file.flush();  // Tatsächlich Speichern, wäre ansonsten evtl. nur im Buffer, was zu Fehlern führen kann :(
   }
 }
 
 void error(int errorCode) {
-  Serial.println(errorCode); // Fehler code zum Debuggen an den Laptop schicken
+  Serial.println(errorCode);  // Fehler code zum Debuggen an den Laptop schicken
 
   // Dauerhaftes Fehler-Piepen und blinkende LED
-  byte countdown = 60; // 60 Sekunden = 1 Minute
+  byte countdown = 60;  // 60 Sekunden = 1 Minute
   while (countdown > 0) {
     digitalWrite(LED_PIN, HIGH);
-    tone(SPEAKER_PIN, 1000, 500);
+    //tone(SPEAKER_PIN, 1000, 500);
     delay(500);
     digitalWrite(LED_PIN, LOW);
     delay(500);
